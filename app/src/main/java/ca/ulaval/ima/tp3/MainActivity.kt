794a8716c6902f98db0710485e89fd7712898315
Cl0ut1eR <etiennecloutier00@gmail.com>
2023-03-31 21:55:28 -0400

    last commit j'ai poché :(

--- app/src/main/java/ca/ulaval/ima/tp3/MainActivity.kt
+++ app/src/main/java/ca/ulaval/ima/tp3/MainActivity.kt
@@ -1,15 +1,16 @@
 package ca.ulaval.ima.tp3
 
+import android.R
 import android.os.Bundle
-import com.google.android.material.floatingactionbutton.FloatingActionButton
-import com.google.android.material.snackbar.Snackbar
-import com.google.android.material.tabs.TabLayout
-import androidx.viewpager.widget.ViewPager
+import android.widget.ArrayAdapter
+import android.widget.ListView
 import androidx.appcompat.app.AppCompatActivity
-import android.view.Menu
-import android.view.MenuItem
-import ca.ulaval.ima.tp3.ui.main.SectionsPagerAdapter
+import androidx.viewpager.widget.ViewPager
 import ca.ulaval.ima.tp3.databinding.ActivityMainBinding
+import ca.ulaval.ima.tp3.ui.main.SectionsPagerAdapter
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayout.OnTabSelectedListener
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -26,5 +27,29 @@ class MainActivity : AppCompatActivity() {
         viewPager.adapter = sectionsPagerAdapter
         val tabs: TabLayout = binding.tabs
         tabs.setupWithViewPager(viewPager)
+
+      /*  tabs.addOnTabSelectedListener(object : OnTabSelectedListener {
+            override fun onTabSelected(tab: TabLayout.Tab) {
+                // Get the position of the selected tab
+                val position = tab.position
+
+                println(position)
+                // Perform the necessary action to show your layout or fragment
+                if (position == 0) {
+                    // Show the first fragment
+                   /* supportFragmentManager.beginTransaction()
+                        .replace(R.id.fragment_container, FirstFragment())
+                        .commit()*/
+                } else if (position == 1) {
+                    // Show the second layout
+                  //  setContentView(R.layout.offers)
+                } else if (position == 2) {
+                    // Show the third fragment
+                   /* supportFragmentManager.beginTransaction()
+                        .replace(R.id.fragment_container, ThirdFragment())
+                        .commit()*/
+                }
+            }
+        })*/
     }
 }
--- app/src/main/java/ca/ulaval/ima/tp3/ui/main/PlaceholderFragment.kt
+++ app/src/main/java/ca/ulaval/ima/tp3/ui/main/PlaceholderFragment.kt
@@ -1,15 +1,23 @@
 package ca.ulaval.ima.tp3.ui.main
 
+import android.icu.text.SimpleDateFormat
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
+import android.widget.ArrayAdapter
+import android.widget.Toast
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
-import ca.ulaval.ima.tp3.R
 import ca.ulaval.ima.tp3.databinding.FragmentMainBinding
+import java.io.BufferedInputStream
+import java.io.BufferedReader
+import java.io.InputStream
+import java.io.InputStreamReader
+import java.net.HttpURLConnection
+import java.net.URL
+import java.util.*
+
 
 /**
  * A placeholder fragment containing a simple view.
@@ -19,9 +27,11 @@ class PlaceholderFragment : Fragment() {
     private lateinit var pageViewModel: PageViewModel
     private var _binding: FragmentMainBinding? = null
 
+    private var action = "brand"
+
     // This property is only valid between onCreateView and
     // onDestroyView.
-    private val binding get() = _binding!!
+    public val binding get() = _binding!!
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,7 +39,16 @@ class PlaceholderFragment : Fragment() {
             setIndex(arguments?.getInt(ARG_SECTION_NUMBER) ?: 1)
         }
     }
-
+    fun readStream(inputStream: InputStream): String {
+       /* val reader = BufferedReader(InputStreamReader(inputStream))
+        val stringBuilder = StringBuilder()
+        var line: String? = null
+        while ({line = reader.readLine(); line}() != null) {
+            stringBuilder.append(line)
+        }
+        return stringBuilder.toString()*/
+        return "helo"
+    }
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -38,12 +57,65 @@ class PlaceholderFragment : Fragment() {
         _binding = FragmentMainBinding.inflate(inflater, container, false)
         val root = binding.root
 
-        val textView: TextView = binding.sectionLabel
-        pageViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })
+        var mainListView = binding.mainListView
+
+
+        val mutableList = mutableListOf<String>()
+
+        mutableList.add("Brand 1")
+        mutableList.add("Brand 2")
+        mutableList.add("Brand 3")
+        mutableList.add("Brand 4")
+
+
+        /*api tp3.infomobile.app/api/v1/brand/
+        for items
+            brands[name] = id
+            mutableList.add(name)
+         */
+
+
+        val adapter = ArrayAdapter<String>(requireContext(), android.R.layout.simple_list_item_1, mutableList)
+        mainListView.adapter = adapter
+
+
+
+
+
+        mainListView.setOnItemClickListener { parent, view, position, id ->
+            val selectedItemName = parent.getItemAtPosition(position).toString()
+            if(action == "brand") {
+                /*
+            api call tp3.infomobile.app/api/v1/modal/
+            for items
+                modal[name] = id
+                mutableList.add(name)
+             */
+                mutableList.clear()
+                mutableList.add("Model 1")
+                mutableList.add("Model 2")
+                mutableList.add("Model 3")
+                mutableList.add("Model 4")
+
+                val adapter = ArrayAdapter<String>(
+                    requireContext(),
+                    android.R.layout.simple_list_item_1,
+                    mutableList
+                )
+
+                mainListView.adapter = adapter
+
+                action = "modal"
+            }else if(action == "modal"){
+                mutableList.clear()
+
+            }
+
+        }
+
         return root
     }
+
 
     companion object {
         /**
@@ -52,7 +124,7 @@ class PlaceholderFragment : Fragment() {
          */
         private const val ARG_SECTION_NUMBER = "section_number"
 
-        /**
+        /**<
          * Returns a new instance of this fragment for the given section
          * number.
          */
